245b1c21fa62910d92545b4ae066d436833a77f6
peopleAlreadyKnowWhoIAm <nz54ds@gmail.com>
2022-04-15 17:38:08 +0300

    Added test-files for `TextMenu` package

---
+++ src/test/java/ua/lpnu/students/labs/laba2/ChristmassDecoration/TextMenu/ConsoleComunicatorTest.java
@@ -0,0 +1,130 @@
+package ua.lpnu.students.labs.laba2.ChristmassDecoration.TextMenu;
+
+import org.junit.jupiter.api.Test;
+
+public class ConsoleComunicatorTest {
+    @Test
+    void testEdit() {
+
+    }
+
+    @Test
+    void testEditBool() {
+
+    }
+
+    @Test
+    void testEditDate() {
+
+    }
+
+    @Test
+    void testEditFloat() {
+
+    }
+
+    @Test
+    void testEditInt() {
+
+    }
+
+    @Test
+    void testEditSize() {
+
+    }
+
+    @Test
+    void testEditString() {
+
+    }
+
+    @Test
+    void testEditStringList() {
+
+    }
+
+    @Test
+    void testEditUsage() {
+
+    }
+
+    @Test
+    void testEditUsageList() {
+
+    }
+
+    @Test
+    void testPrint() {
+
+    }
+
+    @Test
+    void testScanInt() {
+
+    }
+
+    @Test
+    void testScanStrictlyInt() {
+
+    }
+
+    @Test
+    void testScanString() {
+
+    }
+
+    @Test
+    void testSet() {
+
+    }
+
+    @Test
+    void testSetBool() {
+
+    }
+
+    @Test
+    void testSetDate() {
+
+    }
+
+    @Test
+    void testSetFloat() {
+
+    }
+
+    @Test
+    void testSetInt() {
+
+    }
+
+    @Test
+    void testSetSize() {
+
+    }
+
+    @Test
+    void testSetString() {
+
+    }
+
+    @Test
+    void testSetStringList() {
+
+    }
+
+    @Test
+    void testSetType() {
+
+    }
+
+    @Test
+    void testSetUsage() {
+
+    }
+
+    @Test
+    void testSetUsageList() {
+
+    }
+}
---
+++ src/test/java/ua/lpnu/students/labs/laba2/ChristmassDecoration/TextMenu/TextMenuTest.java
@@ -0,0 +1,25 @@
+package ua.lpnu.students.labs.laba2.ChristmassDecoration.TextMenu;
+
+import org.junit.jupiter.api.Test;
+
+public class TextMenuTest {
+    @Test
+    void testAddDecoration() {
+
+    }
+
+    @Test
+    void testDeleteDecoration() {
+
+    }
+
+    @Test
+    void testEditDecoration() {
+
+    }
+
+    @Test
+    void testMainMenu() {
+
+    }
+}
